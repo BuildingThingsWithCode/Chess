975217bd6a1bf44c099292542e42efc5c99d5648
BuildingThingsWithCode <87835378+BuildingThingsWithCode@users.noreply.github.com>
2021-07-24 21:45:14 +0200

    Update Sounds.java
    fix indentation

--- src/chessApplication/Sounds.java
+++ src/chessApplication/Sounds.java
@@ -4,13 +4,13 @@ import javafx.scene.media.AudioClip;
 
 public class Sounds {
 	
-	private final AudioClip MOVE 				  = new AudioClip(getClass().getResource("../resources/sounds/move.mp3").toString());
-	private final AudioClip DRAW 				  = new AudioClip(getClass().getResource("../resources/sounds/draw.mp3").toString());
-	private final AudioClip CHECKMATE 			  = new AudioClip(getClass().getResource("../resources/sounds/checkmate.mp3").toString());
-	private final AudioClip STALEMATE 			  = new AudioClip(getClass().getResource("../resources/sounds/stalemate.mp3").toString());
-	private final AudioClip OUT_OF_TIME 		  = new AudioClip(getClass().getResource("../resources/sounds/outoftime.mp3").toString());
+	private final AudioClip MOVE		      = new AudioClip(getClass().getResource("../resources/sounds/move.mp3").toString());
+	private final AudioClip DRAW	              = new AudioClip(getClass().getResource("../resources/sounds/draw.mp3").toString());
+	private final AudioClip CHECKMATE	      = new AudioClip(getClass().getResource("../resources/sounds/checkmate.mp3").toString());
+	private final AudioClip STALEMATE	      = new AudioClip(getClass().getResource("../resources/sounds/stalemate.mp3").toString());
+	private final AudioClip OUT_OF_TIME	      = new AudioClip(getClass().getResource("../resources/sounds/outoftime.mp3").toString());
 	private final AudioClip INSUFFICIENT_MATERIAL = new AudioClip(getClass().getResource("../resources/sounds/insufficientmaterial.mp3").toString());	
-	private final AudioClip RESIGN 				  = new AudioClip(getClass().getResource("../resources/sounds/resign.mp3").toString());
+	private final AudioClip RESIGN 		      = new AudioClip(getClass().getResource("../resources/sounds/resign.mp3").toString());
 	
 	//CONSTRUCTOR
 	public Sounds() {}
