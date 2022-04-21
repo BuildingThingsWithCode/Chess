d910c9ebfafb4ecaca5ec8d45f12bba6ee2f9fe8
Lars Martens <larsmartens2@gmail.com>
2022-04-22 00:33:15 +0200

    made sounds play in separate thread

--- src/chessApplication/Sounds.java
+++ src/chessApplication/Sounds.java
@@ -3,44 +3,53 @@ package chessApplication;
 import javafx.scene.media.AudioClip;
 
 public class Sounds {
-	
-	private final AudioClip MOVE                  = new AudioClip(getClass().getResource("../resources/sounds/move.mp3").toString());
-	private final AudioClip DRAW                  = new AudioClip(getClass().getResource("../resources/sounds/draw.mp3").toString());
-	private final AudioClip CHECKMATE             = new AudioClip(getClass().getResource("../resources/sounds/checkmate.mp3").toString());
-	private final AudioClip STALEMATE             = new AudioClip(getClass().getResource("../resources/sounds/stalemate.mp3").toString());
-	private final AudioClip OUT_OF_TIME           = new AudioClip(getClass().getResource("../resources/sounds/outoftime.mp3").toString());
-	private final AudioClip INSUFFICIENT_MATERIAL = new AudioClip(getClass().getResource("../resources/sounds/insufficientmaterial.mp3").toString());	
-	private final AudioClip RESIGN                = new AudioClip(getClass().getResource("../resources/sounds/resign.mp3").toString());
-	
-	//CONSTRUCTOR
-	public Sounds() {}
-	
-	//METHODS
-	public void move() {
-		MOVE.play();
-	}
-	
-	public void draw() {
-		DRAW.play();
-	}
-	
-	public void checkmate() {
-		CHECKMATE.play();
-	}
 
-	public void stalemate() {
-		STALEMATE.play();
-	}
-	
-	public void outOfTime() {
-		OUT_OF_TIME.play();
-	}
-	
-	public void insufficientMaterial() {
-		INSUFFICIENT_MATERIAL.play();
-	}
-	
-	public void resign() {
-		RESIGN.play();
-	}
+   private final AudioClip move                  = new AudioClip(getClass().getResource("../resources/sounds/move.mp3").toString());
+   private final AudioClip draw                  = new AudioClip(getClass().getResource("../resources/sounds/draw.mp3").toString());
+   private final AudioClip checkmate             = new AudioClip(getClass().getResource("../resources/sounds/checkmate.mp3").toString());
+   private final AudioClip stalemate             = new AudioClip(getClass().getResource("../resources/sounds/stalemate.mp3").toString());
+   private final AudioClip outOfTime             = new AudioClip(getClass().getResource("../resources/sounds/outoftime.mp3").toString());
+   private final AudioClip insufficientMaterial  = new AudioClip(getClass().getResource("../resources/sounds/insufficientmaterial.mp3").toString());	
+   private final AudioClip resign                = new AudioClip(getClass().getResource("../resources/sounds/resign.mp3").toString());
+
+   //CONSTRUCTOR
+   public Sounds() {}
+   
+   //METHODS
+   private void play(AudioClip clip) {
+      new Thread(new Runnable() {
+         public void run() {
+            clip.play();
+         }
+      }).start();
+   }
+   
+   public void move() {
+      play(move);
+
+   }
+
+   public void draw() {
+      play(draw);
+   }
+
+   public void checkmate() {
+      play(checkmate);
+   }
+
+   public void stalemate() {
+      play(stalemate);
+   }
+
+   public void outOfTime() {
+      play(outOfTime);
+   }
+
+   public void insufficientMaterial() {
+      play(insufficientMaterial);
+   }
+
+   public void resign() {
+     play(resign);
+   }
 }
